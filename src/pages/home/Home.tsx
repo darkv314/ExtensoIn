d065a97876434fc62f71e6d071648f3aab8038d0
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-16 12:18:02 -0400

    base landing page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,25 @@
+import Description from "../../components/text/description/Description";
 import Title from "../../components/text/tittle/Title";
 import "./Home.css";
+import banner from "../../assets/banner-1.jpg";
 
 function Home() {
-
-    return <>
-    <Title text="Home" />
-    </>;
+  return (
+    <>
+      <section className="first__section">
+        <div className="div__left">
+          <Title text="InExperto" />
+          <Description
+            title="Donde la pasión supera a la experiencia"
+            content="Conecta a empleadores dispuestos a brindar oportunidades de crecimiento con buscadores de empleo ambiciosos y motivados. InExperto se enfoca en el potencial, las habilidades transferibles y la pasión de cada individuo, proporcionándoles una plataforma segura para crear perfiles y presentar sus habilidades. Es un lugar donde la experiencia no es un requisito, y donde se promueve la diversidad, la inclusión y el desarrollo profesional."
+          />
+        </div>
+        <div className="div__right">
+          <img width={400} src={banner} />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Home;

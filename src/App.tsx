f3aa207d47ebc6139908985b87893f87b630988a
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-16 15:05:27 -0400

    grid users feed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Layout from "./views/layout/Layout";
+import Feed from "./pages/feed/Feed";
 
 function App() {
-    return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/home" element={<Home />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/feed" element={<Feed />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/feed/Feed.tsx
@@ -0,0 +1,15 @@
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <>
+      <section className="grid__feed">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </section>
+    </>
+  );
+}
+
+export default Feed;

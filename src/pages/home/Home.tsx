ba5da96964e93da1d70a51488f0447e66a93ec93
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-16 11:27:09 -0400

    update styles navbar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../../views/navbar/NavBar";
 import "./Home.css";
+import { LargeSuitcase } from "iconoir-react";
+import { Send } from "iconoir-react";
 
 function Home() {
   const links = [
-    { text: "Trabajos", url: "/works" },
-    { text: "Publicar", url: "/publish" },
-    { text: "Unirse", url: "/sign-in" },
-    { text: "Registrarse", url: "/log-in" },
+    { text: "Trabajos", url: "/works", icon: <LargeSuitcase /> },
+    { text: "Publicar", url: "/publish", icon: <Send /> },
+    { text: "Unirse", url: "/sign-in", border: true },
+    { text: "Registrarse", url: "/log-in", border: true },
   ];
   return (
     <>
